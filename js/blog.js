95296e1f1f473cf048e64004b2667f9fe9c5f3f8
Riccardo Massidda <contact@rmassidda.it>
2020-04-12 21:42:53 +0200

    Support for the blog posts in the homepage

---
+++ js/blog.js
@@ -0,0 +1,31 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function () {
+	if (xhr.status >= 200 && xhr.status < 300) {
+    posts = xhr.response
+    ul = document.getElementById('archive')
+    /* When splitting by new line the last line is void */
+    for (post of posts.split('\n').slice(0,-1).reverse()){
+      /* Parse of the CSV fields */
+      fields = post.split('","')
+      date   = fields[0].slice(1)
+      title  = fields[1]
+      href   = fields[2]
+      orig   = fields[3].slice(0,-1)
+      /* Node listing creation */
+      li = document.createElement('li')
+      a  = document.createElement('a')
+      a.href = 'post/'+href
+      txt = document.createTextNode(title)
+      a.appendChild(txt)
+      li.appendChild(a)
+      li.innerHTML = date.split(' ')[0] + ', ' + li.innerHTML
+      ul.appendChild(li) 
+    }
+	} else {
+    document.getElementById('archive').innerHTML = 'No posts :('
+	}
+};
+
+xhr.open('GET', 'index.csv');
+xhr.send();
